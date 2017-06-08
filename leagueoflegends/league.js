43343648f2ce7fd4e5dc568dd5f27ca862097c28
Wonder Ginger <brandt.willems@gmail.com>
2017-06-08 13:47:03 -0700

    troubleshooting api

--- leagueoflegends/league.js
+++ leagueoflegends/league.js
@@ -17,7 +17,7 @@ function setup() {
   var response;
   var object2 = {
     url: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wonderginger?api_key=RGAPI-edc09dbb-b009-41ec-bb5c-e8af5f65cad8",
-    dataType: "jsonp",
+    dataType: "string",
     success: function(data) {
       console.log(data);
       response = data;
@@ -26,7 +26,7 @@ function setup() {
   };
   $.ajax({
     url: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wonderginger?api_key=RGAPI-edc09dbb-b009-41ec-bb5c-e8af5f65cad8",
-    dataType: "jsonp",
+    dataType: "string",
     success: function(data) {
       console.log(data);
       response = data;

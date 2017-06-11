322e575816dd24e549bc5b2723c4f1191d1836cf
Brandt Willems <brandt.willems@gmail.com>
2017-06-10 17:37:42 -0700

    Create league.js

--- leagueoflegends/league.js
+++ leagueoflegends/league.js
@@ -14,26 +14,29 @@ function setup() {
   // api_key = config[0].match(/^api key=(.*)/)[1];
   summonerNameRegex = /^[0-9\\p{L} _\\.]+$/;
 
-  var response;
-  var object2 = {
-    url: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wonderginger?api_key=RGAPI-edc09dbb-b009-41ec-bb5c-e8af5f65cad8",
-    dataType: "string",
-    success: function(data) {
-      console.log(data);
-      response = data;
-      return response;
-    }
-  };
-  $.ajax({
-    url: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wonderginger?api_key=RGAPI-edc09dbb-b009-41ec-bb5c-e8af5f65cad8",
-    dataType: "jsonp",
-    success: function(data) {
-      console.log(data);
-      response
-      response = data;
-      return response;
-    }
+  $('body').scroll(function() { 
+    $('#logo').css('bottom', $(this).scrollTop());
   });
+//   var response;
+//   var object2 = {
+//     url: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wonderginger?api_key=RGAPI-edc09dbb-b009-41ec-bb5c-e8af5f65cad8",
+//     dataType: "string",
+//     success: function(data) {
+//       console.log(data);
+//       response = data;
+//       return response;
+//     }
+//   };
+//   $.ajax({
+//     url: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wonderginger?api_key=RGAPI-edc09dbb-b009-41ec-bb5c-e8af5f65cad8",
+//     dataType: "jsonp",
+//     success: function(data) {
+//       console.log(data);
+//       response
+//       response = data;
+//       return response;
+//     }
+//   });
 }
 
 function summonerLookUp() {

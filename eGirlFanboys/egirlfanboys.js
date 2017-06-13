2ba99613d548c126c987ba7a9fd1e118a714fe9a
Wonder Ginger <brandt.willems@gmail.com>
2017-06-13 00:32:50 -0700

    added 6 graphs to stats page and altered some landing page css

--- eGirlFanboys/egirlfanboys.js
+++ eGirlFanboys/egirlfanboys.js
@@ -1,3 +1,10 @@
+let data;
+let ctx;
+
+function preload() {
+  data = loadJSON('data.json');
+}
+
 function setup() {
   $('#challonge').challonge('Phoenix_League_season2', {
     subdomain: '',
@@ -7,6 +14,16 @@ function setup() {
     show_final_results: '1',
     show_standings: '1'
   });
+
+  drawChart(data, $('#KDA'), "KDA");
+  drawChart(data, $('#firstbloods'), "First Bloods");
+  drawChart(data, $('#avgcsmin'), "Avg CS / Min");
+  drawChart(data, $('#csdiff10'), "CS Diff @ 10");
+  drawChart(data, $('#goldshare'), "Gold Share");
+  drawChart(data, $('#damagemin'), "Damage / Min");
+  drawChart(data, $('#damageshare'), "Damage Share");
+  drawChart(data, $('#killparticipation'), "Kill Participation");
+
 }
 
 function openTab(evt, tabName) {
@@ -30,17 +47,82 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
+function drawChart(data, element, chartType) {
+  // make this called by a mouse click in "stats"
+  ctx = element;
+  // ctx = createCanvas(400, 400);
+  Chart.defaults.global.defaultFontColor = 'white';
+  Chart.defaults.global.elements.point.radius = 0;
 
-function drawChart() {
-  var query = new google.visualization.Query(
-    'https://docs.google.com/spreadsheets/d/1H6Ej3evkqa8xDHLyHDrVBsA12Dvh__ZEKvPv-2tgtaA/edit?usp=sharing'
-  );
-  query.send(handleQueryResponse);
-}
+  let Average = data.Average;
+  let Katar = data.Katar;
+  let Tapmaster = data.Tapmaster;
+  let WonderGinger = data.WonderGinger;
+  let CliveGildarts = data.CliveGildarts;
+  let YoohooManiac = data.YoohooManiac;
+  let Hopeless0ne = data.Hopeless0ne;
 
-function handleQueryResponse(response) {
-  var data = response.getDataTable();
-  var chart = new google.visualization.ColumnChart(document.getElementById(
-    'columnchart'));
-  chart.draw(data, null);
+  let chartData = [];
+  let averageData = [];
+  for (i in data) {
+    if (data[i].Player !== "Average") {
+      let player = data[i];
+      chartData.push(Math.round(player[chartType] * 10) / 10);
+      averageData.push(Math.round(Average[chartType] * 10) / 10);
+    }
+  }
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Katar", "Tapmaster", "Wonder Ginger", "Clive Gildarts",
+        "Yoohoo Maniac", "Hopeless0ne"
+      ],
+      datasets: [{
+        label: chartType,
+        data: chartData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }, {
+        label: 'League Average',
+        data: averageData,
+        lineCap: 'round',
+        borderColor: 'rgba(200,200,200,.5)',
+        borderDash: [30, 5],
+        spanGaps: true,
+        backgroundColor: 'rgba(255,255,255,1)',
+        fill: false,
+        type: 'line'
+      }]
+    },
+    options: {
+
+      legend: {
+        labels: {
+          fontColor: 'white'
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }

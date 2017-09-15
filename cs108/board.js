9acb783f93d60b9857ec4b1f4abf64ec2d586a26
Brandt Willems <brandt.willems@gmail.com>
2017-09-15 15:06:18 -0700

    creating board

--- cs108/board.js
+++ cs108/board.js
@@ -1,3 +1,24 @@
-var sides;
-var width;
-var height;
+class Board {
+  let shapeSides;
+  let radius = 0;
+
+  constructor(shapeSides = 6, radius = 10) {
+    this.sides = shapeSides;
+    this.radius = radius;
+  }
+
+  function polygon(x, y, radius, npoints) {
+    var angle = TWO_PI / npoints;
+    beginShape();
+    for (var a = 0; a < TWO_PI; a += angle) {
+      var sx = x + cos(a) * radius;
+      var sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+
+  function constructBoard() {
+
+  }
+}
